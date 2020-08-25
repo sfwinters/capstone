8e4a38bc4efb64c44f0ec37fe3421fb286708e22
sfwinters <sarah.f.winters@gmail.com>
2020-08-25 18:05:21 -0400

    finished ship descriptions

--- star-trek/src/app/ferengi-fleet/ferengi-fleet.component.ts
+++ star-trek/src/app/ferengi-fleet/ferengi-fleet.component.ts
@@ -21,8 +21,8 @@ export class FerengiFleetComponent implements OnInit {
       class: 'D\'Kora',
       type: 'Transport',
       owner: 'Ferengi Alliance',
-      photo: '',
-      info: '',
+      photo: '/assets/images/ferengi/krayton.jpg',
+      info: 'The FMS Krayton was a Ferengi Alliance marauder captained by a Ferengi called DaiMon Tog. In 2366, the Krayton represented its government at a trade conference on the planet Betazed. While there, Tog met and instantly became enamored with Lwaxana Troi, mother of Enterprise crew member Deanna Troi. Though Lwaxana did not share his affections and very publicly turned him down, that encouraged him further. He attempted to surprise her while she was speaking to Deanna and Enterprise First Officer Will Riker, which ended in Tog abducting all three of them to the Krayton. Though nearly as fast as the Enterprise, the Krayton did not manage to get away and the three crew members were returned unharmed.',
       id: 'krayton'
     },
     {
@@ -30,8 +30,8 @@ export class FerengiFleetComponent implements OnInit {
       class: 'Large Shuttle',
       type: 'Shuttlepod',
       owner: 'Quark',
-      photo: '',
-      info: '',
+      photo: '/assets/images/ferengi/quark.jpg',
+      info: 'In the year 2372, Ferengi bar owner and entrepreneur Quark received a Ferengi shuttle from his cousin, as a way of repaying a previous loan. Quark named the shuttle "Quark\'s Treasure," and immediately attempted to use it on a smuggling mission accompanied by his brother, Rom, and nephew, Nog. They quickly discovered that Quark\'s cousin had sabotaged the shuttle and while trying to repair it, Rom inadvertently sent Quark\'s Treasure back in time to the year 1947, upon which it promptly crash-landed. Rom was fortunately able to undo his mistake and send the shuttle back to the correct location and time, with the help of Deep Space Nine\'s security chief (who had stowed away onboard, suspecting that Quark was up to something illegal). When they returned, it was determined that the shuttle was totaled and Quark was forced to sell it as salvage.',
       id: 'quark'
     }
     
